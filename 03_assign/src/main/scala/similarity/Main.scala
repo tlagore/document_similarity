725daad62174a2c7e1effe3ca19808a4cdec8ddf
Tyrone Lagore <tyronelagore@gmail.com>
2022-02-23 15:29:39 -0800

    filtered duplicate

--- 03_assign/src/main/scala/similarity/Main.scala
+++ 03_assign/src/main/scala/similarity/Main.scala
@@ -87,7 +87,18 @@ object Main extends App {
     } else
       Array()
 
+  println("JACCARD DOCS!!")
+  println("JACCARD DOCS!!")
+  println("JACCARD DOCS!!")
+  println("JACCARD DOCS!!")
   jac.foreach(el => println(el))
+
+  println("minHashes!!!!")
+  println("minHashes!!!!")
+  println("minHashes!!!!")
+  println("minHashes!!!!")
+  println("minHashes!!!!")
+  minHashes.foreach(el => println(el))
 
   val minHashesMatches: Matches =
     if (DO_ALL_MIN_HASHES) {
@@ -95,6 +106,12 @@ object Main extends App {
       minhash.find_minhash_matches(minHashes, MIN_SIM)
     } else Array()
 
+  println("minHashesMatches!!!!")
+  println("minHashesMatches!!!!")
+  println("minHashesMatches!!!!")
+  println("minHashesMatches!!!!")
+  println("minHashesMatches!!!!")
+  minHashesMatches.foreach(el => println(el))
   System.err.println("Doing LHSs comparisons...")
 
   val lshs = minhash.find_lsh_matches(minHashes, MIN_SIM, BAND_SIZE)
--- 03_assign/src/main/scala/similarity/minhash.scala
+++ 03_assign/src/main/scala/similarity/minhash.scala
@@ -19,8 +19,23 @@ object minhash {
     Shingled_Record(id, count, returnRecords)
   }
 
+  /**
+   * Given a list of hash functions, return a list of minimum hashes such that
+   * element i is the hash_function return with the lowest hash
+   * @param r
+   * @param hashFuncs
+   * @return
+   */
   def minhash_record(r: Shingled_Record, hashFuncs: List[Hash_Func]): Min_Hash_Record = {
-    Min_Hash_Record("id", Vector[Int]())
+    r.shingles match {
+      case Some(shingles) =>
+        val minHashes:Vector[Int] = hashFuncs.foldLeft(Vector[Int]())((op, el) => {
+          op :+ (shingles.map(el).min)
+        })
+
+        Min_Hash_Record(r.id, minHashes)
+      case _ => Min_Hash_Record(r.id, Vector[Int]())
+    }
   }
 
   def compute_jaccard_pair(a: Shingled_Record, b: Shingled_Record): Similarity = {
@@ -35,20 +50,57 @@ object minhash {
   }
 
   def find_jaccard_matches(records: RDD[Shingled_Record], minSimilarity: Double): Matches = {
-    records.cartesian(records).filter((el) => el._1 != el._2).aggregate(Array[Similarity]())(
+    val matches = records.cartesian(records).filter(el => el._1 != el._2).aggregate(Array[Similarity]())(
       (accumulator, el) => {
         val score = compute_jaccard_pair(el._1, el._2)
         if (score.sim > minSimilarity) accumulator :+ score else accumulator
       },
       (l1,l2) => l1++l2
     )
+
+    filter_duplicates(matches)
+  }
+
+  def min_hash_similarity(a: Min_Hash_Record, b: Min_Hash_Record): Similarity = {
+    val sameEls = a.minHashes.zip(b.minHashes).foldLeft(0)((op, pair) =>
+        if (pair._1 == pair._2) op + 1 else op
+      )
+    val similarity:Float = sameEls.toFloat / a.minHashes.length
+    Similarity(a.id, b.id, similarity)
+  }
+
+  def filter_duplicates(records: Matches): Matches = {
+    // filter out double pairs. I.e. if we have (a,b), don't include (b,a)
+
+    val (retMatches, _) = records.foldLeft((Array[Similarity](), Set[(String, String)]()))((op, el) => {
+      if (op._2.contains((el.idA, el.idB)) || op._2.contains((el.idB, el.idA)))
+        op
+      else
+        (op._1 :+ el, op._2 + ((el.idA, el.idB)))
+    })
+
+    retMatches
   }
 
   def find_minhash_matches(records: RDD[Min_Hash_Record], minSimilarity: Double): Matches = {
-    new Array[Similarity](1)
+    val matches = records.cartesian(records).filter(el => el._1 != el._2).aggregate(Array[Similarity]())(
+      (accumulator, el) => {
+        if (el._1.id == "t1088" && el._2.id == "t5015")
+          println("here")
+        val similarity = min_hash_similarity(el._1, el._2)
+        if (similarity.sim > minSimilarity)
+          accumulator :+ similarity
+        else
+          accumulator
+      },
+      (l1, l2) => l1++l2
+    )
+
+    filter_duplicates(matches)
   }
 
-  def find_lsh_matches(minHashes: RDD[Min_Hash_Record], minSimilarity: Double, bandSize: Int): Matches = {
-    new Array[Similarity](1)
+  def find_lsh_matches(minHashes: RDD[Min_Hash_Record], minSimilarity: Double, bandSize: Int): Matches = {x
+    Array[Similarity]()
   }
 }
+
